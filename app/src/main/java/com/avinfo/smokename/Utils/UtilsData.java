2d36e0a91d98b5d16f8a4ab28a9b82b65846cda8
milanbhut <milanbhut7199@gmail.com>
2022-04-15 11:21:07 +0530

    commit id

--- app/src/main/java/com/avinfo/smokename/Utils/UtilsData.java
+++ app/src/main/java/com/avinfo/smokename/Utils/UtilsData.java
@@ -6,8 +6,8 @@ public class UtilsData {
 //    public static String FB_Interstitial_AD = "YOUR_PLACEMENT_ID";
 //    public static String FB_Native_AD = "YOUR_PLACEMENT_ID";
 
-    public static String FB_Banner_AD = "2609271965761607_2609276219094515";
-    public static String FB_Interstitial_AD = "2609271965761607_2609277335761070";
-    public static String FB_Native_AD = "2609271965761607_2609273999094737";
+    public static String FB_Banner_AD = "YOUR_APP_ID";
+    public static String FB_Interstitial_AD = "YOUR_APP_ID";
+    public static String FB_Native_AD = "YOUR_APP_ID";
 
 }
